5c9fb966e59b838c78064b6160ad72d998db97bc
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2024-11-14 23:31:15 +0300

    Project folders and files have been created. Bubble Sort section has been completed.

---
+++ Arrays/improved_bubble_sort.py
@@ -0,0 +1,11 @@
+def improved_bubble_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        swapped = False
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
---
+++ Arrays/main.py
@@ -0,0 +1,9 @@
+from bubble_sort import bubble_sort
+from improved_bubble_sort import improved_bubble_sort
+
+my_array1 = [64, 34, 25, 12, 22, 11, 90, 5]
+my_array2 = [7, 3, 9, 12, 11]
+
+sorted_array = bubble_sort(my_array2)
+
+print(sorted_array)
